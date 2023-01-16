a82e60d222333eb8105f9ed824e5a432bfa84fed
DESKTOP-U804JBD\Hercules PC <hercprogramming@gmail.com>
2023-01-16 11:32:18 +0000

    Initialize app + components + bootstrap

---
+++ src/app/newsfeed/newsfeed.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sleep-share-newsfeed',
+  templateUrl: './newsfeed.component.html',
+  styleUrls: ['./newsfeed.component.css'],
+})
+export class NewsfeedComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
